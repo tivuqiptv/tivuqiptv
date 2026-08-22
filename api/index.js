fd11038a1605d9cb14aac8061857501d3b5e9e48
ümit <umit@Mac.home>
2026-08-23 01:21:32 +0200

    Fix import path to server.js in root api/index.js

--- api/index.js
+++ api/index.js
@@ -1,4 +1,4 @@
-import { requestHandler } from './server/src/server.js';
+import { requestHandler } from '../server/src/server.js';
 
 export default async function handler(req, res) {
   const originalPath = req.headers['x-matched-path'] || req.headers['x-vercel-matched-path'] || req.headers['x-forwarded-uri'] || req.headers['x-original-uri'];
